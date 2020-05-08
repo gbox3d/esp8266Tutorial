69aa379fa4a9de461f6220bead2881d12d8807bd
seokjun lee <gbox3d@seokjunui-MacBookPro.local>
2020-05-08 15:18:35 +0900

    udp tester

--- net_tools/udp_sender.js
+++ net_tools/udp_sender.js
@@ -1,21 +1,24 @@
-
+//node udp_sender.js --ip=192.168.4.2 --port 2012 --content
 var dgram = require( "dgram" );
+const yargs = require('yargs').argv;
 
 var client = dgram.createSocket( "udp4" );
 
 client.on( "message", function( msg, rinfo ) {
+    console.log(rinfo)
     console.log( "The packet came back" );
     console.log(msg.toString());
-    console.log(rinfo)
+
+    client.close()
 });
 
 // client listens on a port as well in order to receive ping
 client.bind(); //포트를 특정하지 않으면 모든 포트의 데이터를 받는다.
 
-var message = new Buffer.from( "hello udp!" );
+var message = new Buffer.from( yargs.content );
 client.send(
     message, 0,
     message.length,
-    4210,"192.168.4.24" );
+    yargs.port,yargs.ip);
 
 console.log('send data');
--- net_tools/udp_server.js
+++ net_tools/udp_server.js
@@ -10,24 +10,28 @@ server.on( "message", function( msg, rinfo ) {
     console.log(rinfo);
     console.log(msg);
 
-    console.log(`header : ${msg.readUInt8(0) } `);
-    console.log(`ceode : ${msg.readUInt8(1) } `);
-    console.log(`m_index : ${msg.readInt16LE(4) } `);
-    console.log(`m_iSensorData : ${msg.readFloatLE(8) } `);
+    let message = new Buffer.from(`ok : ${msg}`);
 
-    let resBuf = Buffer.alloc(8)
-    resBuf.writeUInt8(77,0);
-    resBuf.writeUInt8(49,1); 
-    resBuf.writeUInt8(36,2); //m_status
-    resBuf.writeUInt8(18,3); //m_extra
+    server.send(message,0,message.length,rinfo.port,rinfo.address)
 
-    resBuf.writeInt16LE(516,4)
+    // console.log(`header : ${msg.readUInt8(0) } `);
+    // console.log(`ceode : ${msg.readUInt8(1) } `);
+    // console.log(`m_index : ${msg.readInt16LE(4) } `);
+    // console.log(`m_iSensorData : ${msg.readFloatLE(8) } `);
 
-    console.log(resBuf)
+    // let resBuf = Buffer.alloc(8)
+    // resBuf.writeUInt8(77,0);
+    // resBuf.writeUInt8(49,1); 
+    // resBuf.writeUInt8(36,2); //m_status
+    // resBuf.writeUInt8(18,3); //m_extra
 
-    server.send(
-        resBuf, 0, 8,
-        rinfo.port, rinfo.address ); // added missing bracket
+    // resBuf.writeInt16LE(516,4)
+
+    // console.log(resBuf)
+
+    // server.send(
+    //     resBuf, 0, 8,
+    //     rinfo.port, rinfo.address ); // added missing bracket
 });
 
 server.bind(localPort)
