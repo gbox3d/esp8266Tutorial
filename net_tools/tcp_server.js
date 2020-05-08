b822b1f8a362f5c4e09eed6e681f65ec7c6401cd
gbox3 <gbox3d@gmail.com>
2020-05-08 15:04:07 +0900

    server test

--- net_tools/tcp_server.js
+++ net_tools/tcp_server.js
@@ -5,29 +5,33 @@ const yargs = require('yargs').argv;
 //var HOST = '192.168.0.7';
 var PORT = yargs.port;
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(socket) {
+net.createServer(function (_socket) {
+    //클로져 
+    (function (socket) {
 
-    // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
+        // We have a connection - a socket object is assigned to the connection automatically
+        console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
 
-    // Add a 'data' event handler to this instance of socket
-    socket.on('data', function(data) {
+        // Add a 'data' event handler to this instance of socket
+        socket.on('data', function (data) {
 
-        console.log('DATA ' + socket.remoteAddress + ': ' + data);
-        console.log(data);
-        // Write the data back to the socket, the client will receive it as data from the server
-        socket.write('You said "' + data + '"' + "\n");
+            console.log('DATA ' + socket.remoteAddress + ': ' + data);
+            console.log(data);
+            // Write the data back to the socket, the client will receive it as data from the server
+            socket.write(`You said : ${data}`);
+            // socket.end();
+        });
 
-    });
+        // Add a 'close' event handler to this instance of socket
+        socket.on('close', function (data) {
+            console.log('CLOSED: ');
+            //console.log(JSON.stringify(socket.getConnection()));
+        });
 
-    // Add a 'close' event handler to this instance of socket
-    socket.on('close', function(data) {
-        console.log('CLOSED: ');
-        //console.log(JSON.stringify(socket.getConnection()));
-    });
+
+    })(_socket)
+
+
 
 }).listen(PORT);
 
