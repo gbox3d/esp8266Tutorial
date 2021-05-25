2ed3dccdad59bc47cf2b92b36dd9e333a722659d
gbox3d <gbox3d@gmail.com>
2021-05-25 13:40:06 +0900

    ...

--- net_tools/udp_sender.js
+++ net_tools/udp_sender.js
@@ -1,13 +1,24 @@
 //node udp_sender.js --ip=192.168.4.2 --port 2012 --content
-var dgram = require( "dgram" );
+//node udp_sender.js --ip=192.168.4.2 --port=6009 --content read-config
+var dgram = require("dgram");
 const yargs = require('yargs').argv;
 
-var client = dgram.createSocket( "udp4" );
+var client = dgram.createSocket("udp4");
 
-client.on( "message", function( msg, rinfo ) {
+client.on("message", function (msg, rinfo) {
     console.log(rinfo)
-    console.log( "The packet came back" );
-    console.log(msg.toString());
+    console.log("The packet came back");
+    console.log(msg);
+
+    let _header = msg.readInt32LE(0) 
+    let _code = msg.readUInt8(8)
+    
+    if (_code === 0x21 && _header == 20200519)  
+    {
+        let _headerSize = 16
+        console.log( msg.slice(_headerSize, msg.length).toString('utf-8') )
+    }
+    
 
     client.close()
 });
@@ -15,10 +26,26 @@ client.on( "message", function( msg, rinfo ) {
 // client listens on a port as well in order to receive ping
 client.bind(); //포트를 특정하지 않으면 모든 포트의 데이터를 받는다.
 
-var message = new Buffer.from( yargs.content );
-client.send(
-    message, 0,
-    message.length,
-    yargs.port,yargs.ip);
+_ip = yargs.ip
+_port = yargs.port
+console.log(yargs.content)
 
-console.log('send data');
+if (yargs.content === 'read-config') {
+    let resBuf = Buffer.alloc(8)
+    resBuf.writeInt32LE(20200519, 0);
+    resBuf.writeUInt8(0x21, 4);
+    resBuf.writeUInt8(0x1, 5); // 1 이면 요청한주소로 응답요청 ,0 이면 지정된 원격서버로 전달
+    resBuf.writeUInt8(0x0, 6);
+
+    console.log(`send : ${_ip}, ${_port}`)
+    console.log(resBuf)
+    client.send(resBuf, 0, resBuf.length, _port, _ip)
+}
+else {
+    var message = new Buffer.from(yargs.content);
+    console.log(`send : ${_ip}, ${_port}`)
+    client.send(
+        message, 0,
+        message.length,
+        _port,_ip);
+}
--- net_tools/udp_server.js
+++ net_tools/udp_server.js
@@ -1,3 +1,4 @@
+//node udp_sender.js --ip=localhost --port=6009 --content read-config
 const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 const yargs = require('yargs').argv;
@@ -10,35 +11,39 @@ server.on("message", function (msg, rinfo) {
 
     console.log(rinfo);
 
-    let header = msg.readUInt8(0)
+    let header = msg.readInt32LE(0)
 
-    if (header == 99 && msg.length >= 12) {
-        console.log(`header : ${msg.readUInt8(0)} `);
-        console.log(`ceode : ${msg.readUInt8(1)} `);
-        console.log(`m_index : ${msg.readInt16LE(4)} `);
+    // console.log("header : " + header )
+    console.log(`header ${header} , length ${msg.length}`)
+
+    if (header == 99) {
+        console.log(`header : ${header} `);
+        console.log(`code : ${msg.readUInt8(4)} `);
+        console.log(`m_index : ${msg.readInt16LE(6)} `);
         console.log(`m_iSensorData : ${msg.readFloatLE(8)} `);
 
-        let resBuf = Buffer.alloc(8)
-        resBuf.writeUInt8(77, 0);
-        resBuf.writeUInt8(49, 1);
-        resBuf.writeUInt8(36, 2); //m_status
-        resBuf.writeUInt8(18, 3); //m_extra
+        let resBuf = Buffer.alloc(10)
+        resBuf.writeUInt32LE(77, 0);
+        
+        resBuf.writeUInt8(49, 4);
+        resBuf.writeUInt8(36, 5); //m_status
 
-        resBuf.writeInt16LE(516, 4)
+        resBuf.writeUInt16LE(18, 6); //m_extra
+        resBuf.writeInt16LE(516, 8)
 
-        console.log(`send ${resBuf} , ${rinfo.address}:${rinfo.port}`  )
+        console.log(` ${rinfo.address}:${rinfo.port}`  )
+        console.log(resBuf)
 
         server.send(
-            resBuf, 0, 8,
+            resBuf, 0, 10,
             rinfo.port, rinfo.address); // added missing bracket
-
     }
     else {
         
-        console.log(msg);
-
         let message = new Buffer.from(`ok : ${msg}`);
         server.send(message, 0, message.length, rinfo.port, rinfo.address)
+
+        console.log(msg.toString());
     }
 
 
